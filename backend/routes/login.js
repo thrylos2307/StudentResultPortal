7de8cff21f2b01cc2617711a33903be80702dddb
thrylos2307 <nishu123tushar@gmail.com>
2021-04-12 15:34:20 +0530

    pghead

--- backend/routes/login.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const position_Controller = require('../controllers/login.js');
-router.post('/welcome', position_Controller.create);
-module.exports = router;
--- backend/routes/timetable.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const position_Controller = require('../controllers/timetable.js');
-router.post('/timtable', position_Controller.create);
-module.exports = router;
